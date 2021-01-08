e7c3920baac1d4c17de3536e0a74c656482f42b4
ATGSilva <56297617+ATGSilva@users.noreply.github.com>
2021-01-08 19:36:12 +0000

    Depreciated Python/Cython Versions
    Python and Cython versions depreciated as they will no longer be updated for this project. These versions are too slow to be useful.

--- setup_Boid.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-
-setup(
-	include_dirs=[numpy.get_include()],
-    ext_modules = cythonize("boid_lib.pyx", compiler_directives={'language_level' : "3", 'infer_types': True})
-)

18513093b5cf8d394663dd705b770d46289e7f95
ATGSilva <56297617+ATGSilva@users.noreply.github.com>
2021-02-15 16:26:41 +0000

    Cleanup
    Removed Depreciated Versions

--- depreciated/cpp/Forces.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void CohereForce(Boid&);
-void SepForce(Boid&);
-void AlignForce(Boid&);
-void WallForce(Boid&);
--- depreciated/cpp_par/Forces.h
+++
@@ -1,9 +0,0 @@
-// -*- adamgillard-cpp -*-
-#pragma once
-
-void CohereForce(Boid&);
-void SepForce(Boid&);
-void AlignForce(Boid&);
-void WallForce(Boid&);
-Vec3D RandWindForce();
-Vec3D WindEvo(Vec3D);
